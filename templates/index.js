37ffae996562d806b432c9d94097b07f275ffeca
bbw <1526699702@qq.com>
2024-01-26 14:03:27 +0800

    fix: 修改地址

--- templates/index.js
+++ templates/index.js
@@ -2,7 +2,7 @@
  * @Author: bbw 1526699702@qq.com
  * @Date: 2024-01-24 17:07:48
  * @LastEditors: bbw 1526699702@qq.com
- * @LastEditTime: 2024-01-26 11:58:58
+ * @LastEditTime: 2024-01-26 14:02:50
  * @FilePath: \create-app-cil\templates\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,8 +13,8 @@ export const templates = {
     description: 'vue模版'
   },
   'vue3-templates-notUI' : {
-    url: 'https://gitee.com/bubowen/vue3-templates-not-ui',
-    downloadUrl: 'https://gitee.com:bubowen/vue3-templates-not-ui#main',
+    url: 'https://gitee.com/bubowen/vue3-templates-notUI',
+    downloadUrl: 'https://gitee.com:bubowen/vue3-templates-notUI#main',
     description: 'vue模版'
   }
 }

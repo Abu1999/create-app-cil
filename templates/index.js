ef157eb0833cbf271d1f90f172ffbe0896a51009
bbw <1526699702@qq.com>
2024-01-26 13:53:59 +0800

    fix: 修改地址

--- templates/index.js
+++ templates/index.js
@@ -2,19 +2,19 @@
  * @Author: bbw 1526699702@qq.com
  * @Date: 2024-01-24 17:07:48
  * @LastEditors: bbw 1526699702@qq.com
- * @LastEditTime: 2024-01-26 11:26:32
+ * @LastEditTime: 2024-01-26 11:58:58
  * @FilePath: \create-app-cil\templates\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export const templates = {
   'vue3-templates' : {
-    url: ' https://gitee.com/bubowen/vue3-templates',
-    downloadUrl: 'https://github.com:Abu1999/vue3-templates#main',
+    url: 'https://gitee.com/bubowen/vue3-templates',
+    downloadUrl: 'https://gitee.com:bubowen/vue3-templates#main',
     description: 'vue模版'
   },
   'vue3-templates-notUI' : {
-    url: ' https://gitee.com/bubowen/vue3-templates-not-ui',
-    downloadUrl: ' https://gitee.com/bubowen/vue3-templates-not-uiI#main',
+    url: 'https://gitee.com/bubowen/vue3-templates-not-ui',
+    downloadUrl: 'https://gitee.com:bubowen/vue3-templates-not-ui#main',
     description: 'vue模版'
   }
 }
